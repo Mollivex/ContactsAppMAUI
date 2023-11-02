a10b9434c4794bf9044e26cba74c33bf87155d76
Alexandr Cocier <Limos12345@outlook.com>
2023-11-02 07:38:16 +0200

    Basics of ListView and Data Binding

--- Contacts.MAUI/Views/ContactsPage.xaml.cs
+++ Contacts.MAUI/Views/ContactsPage.xaml.cs
@@ -5,15 +5,21 @@ public partial class ContactsPage : ContentPage
 	public ContactsPage()
 	{
 		InitializeComponent();
+
+		List<Contact> contacts = new List<Contact>()
+		{
+			new Contact { Name="John Doe", Email="JohnDoe@gmail.com"},
+			new Contact { Name="Jane Duglas", Email="JaneDuglas@gmail.com"},
+			new Contact { Name="Tom Hanks", Email="TomHanks@gmail.com"},
+			new Contact { Name="Frank Liu", Email="FrankLiu@gmail.com"}
+		};
+
+		listContacts.ItemsSource = contacts;
 	}
 
-    private void btnEditContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(EditContactPage));
-    }
-
-    private void btnAddContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(AddContactPage));
-    }
+	public class Contact
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+	}
 }
